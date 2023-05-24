bfbd94f5112be8d303372c4c4f64ab0daac578c2
Rafael Silva <rafaelsilva106@outlook.com>
2023-05-23 21:20:11 -0300

    Modificacoes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,23 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { Container } from './styles';
 import { FaBars } from 'react-icons/fa';
-import Sidebar from '../Sidebar'
-
-
+import Sidebar from '../Sidebar';
+import './estilos.css'; // Importe o arquivo CSS
 
 const Header = () => {
-    const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-    const showSiderbar = () => setSidebar(!sidebar)
+  const showSiderbar = () => setSidebar(!sidebar);
 
-    return(
-        <Container>
-            <FaBars onClick={showSiderbar}/>
-            {sidebar && <Sidebar active={setSidebar} />}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <h1 className="h1">
+        <span>Tw</span>Info
+      </h1>
+      <FaBars onClick={showSiderbar} />
+      {sidebar && <Sidebar active={setSidebar} />}
+    </Container>
+  );
+};
+
 export default Header;

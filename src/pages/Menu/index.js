332d466e96e0bffc2ed83c2c8e0615aa6f5acaa0
Guilherme Nemeti <guinemeti@gmail.com>
2023-06-13 15:46:40 -0300

    alteraçoes arquivo imgs.css

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -9,48 +9,52 @@ import gtav from '../../img/Gta.png';
 import lol from '../../img/Lol.png';
 import rocketleague from '../../img/Rocket.png';
 import r6 from '../../img/R6.png';
+import './imgs.css';
 
 function Menu() {
   return (
-    <div>
-      <h2>
-        <Link to="/menu/csgo">
-          <img src={csgo} alt='descrição' width={290} />
-        </Link>
+    
+    <div className="listarea">
+      <div className="itens">
+        <h2>
+          <Link to="/menu/csgo">
+            <img src={csgo} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/fortnite">
-          <img src={fornite} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/fortnite">
+            <img src={fornite} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/valorant">
-          <img src={valorant} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/valorant">
+            <img src={valorant} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/warzone">
-          <img src={warzone} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/warzone">
+            <img src={warzone} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/fifa">
-          <img src={fifa} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/fifa">
+            <img src={fifa} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/gtav">
-          <img src={gtav} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/gtav">
+            <img src={gtav} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/leagueoflegends">
-          <img src={lol} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/leagueoflegends">
+            <img src={lol} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/rocketleague">
-          <img src={rocketleague} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/rocketleague">
+            <img src={rocketleague} alt='descrição' width={290} />
+          </Link>
 
-        <Link to="/menu/rainbowsix">
-          <img src={r6} alt='descrição' width={290} />
-        </Link>
+          <Link to="/menu/rainbowsix">
+            <img src={r6} alt='descrição' width={290} />
+          </Link>
 
-      </h2>
+        </h2>
+      </div>
     </div>
   );
 }
